bed9098679c6c6c74b37ac80d0ae1040b19be24f
Andrew <mykge@outlook.com>
2019-01-28 20:44:33 +0800

    add Compatibility Tests

--- test/PasswordHasherTest/PasswordHasherTests.cs
+++ test/PasswordHasherTest/PasswordHasherTests.cs
@@ -10,6 +10,8 @@ namespace PasswordHasherTest
     [TestClass]
     public class PasswordHasherTests
     {
+        private const string Pbkdf2Compatibility = "AQAAAAEAACcQAAAAELJDOTDL/u80qJGRkZbmncbo/pEM+f9BRFSikvHvUZPem/GlJ5E8J4fbih3yoU+54w==";
+        private const string Aes256Compatibility = "AgAAAAIAAAABbUaexCPOHjeIFtOw8DNefLq7BQR6gHSodyfCCyzTARpKJfXEJRMnev7NnVnO5ipHLKUr2cegnQ0bzlEUrRXksVS5O/vYhJix5YQ5FLTt85wflKD08NfC/Z8kCC52xtMz";
         private const string TestPassword = "TestPassword1234567890!@#$%^*&(()(__)";
         private static readonly IEnumerable<IPasswordFormatHasher> passwordFormatHashers
             = new List<IPasswordFormatHasher>
@@ -24,8 +26,16 @@ namespace PasswordHasherTest
         public void Pbkdf2WthBase64()
         {
             var passwordHasher = new PasswordHasher(binaryConverter, secureRandomGenerator, passwordFormatHashers);
-            var hashedPassword = passwordHasher.HashPassword(TestPassword, FormatMarkers.Pbkdf2);
+            var hashedPassword = passwordHasher.HashPassword(TestPassword, FormatMarkers.Pbkdf2);           
             var flag = passwordHasher.VerifyHashedPassword(hashedPassword, TestPassword);
+            Assert.IsTrue(flag);
+        }
+
+        [TestMethod]
+        public void Pbkdf2WithBase64Compatibility()
+        {
+            var passwordHasher = new PasswordHasher(binaryConverter, secureRandomGenerator, passwordFormatHashers);
+            var flag = passwordHasher.VerifyHashedPassword(Pbkdf2Compatibility, TestPassword);
             Assert.IsTrue(flag);
         }
 
@@ -35,6 +45,14 @@ namespace PasswordHasherTest
             var passwordHasher = new PasswordHasher(binaryConverter, secureRandomGenerator, passwordFormatHashers);
             var hashedPassword = passwordHasher.HashPassword(TestPassword, FormatMarkers.Aes256);
             var flag = passwordHasher.VerifyHashedPassword(hashedPassword, TestPassword);
+            Assert.IsTrue(flag);
+        }
+
+        [TestMethod]
+        public void Aes256WithBase64Compatibility()
+        {
+            var passwordHasher = new PasswordHasher(binaryConverter, secureRandomGenerator, passwordFormatHashers);
+            var flag = passwordHasher.VerifyHashedPassword(Aes256Compatibility, TestPassword);
             Assert.IsTrue(flag);
         }
     }
