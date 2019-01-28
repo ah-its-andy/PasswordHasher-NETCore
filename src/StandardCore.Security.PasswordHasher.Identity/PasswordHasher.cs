45e00ff236b0f6a7f3a98738500d608516be7306
Andrew <mykge@outlook.com>
2019-01-28 15:01:35 +0800

    add Microsoft Identity Lib Support

---
+++ src/StandardCore.Security.PasswordHasher.Identity/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using IStandardCoreHasher = StandardCore.Security.PasswordHasher.IPasswordHasher;
+
+namespace StandardCore.Security.PasswordHasher.Identity
+{
+    public class PasswordHasher<TUser> : IPasswordHasher<TUser> where TUser : class
+    {
+        private readonly IStandardCoreHasher _passwordHasher;
+
+        public PasswordHasher(IStandardCoreHasher passwordHasher)
+        {
+            _passwordHasher = passwordHasher ?? throw new ArgumentNullException(nameof(passwordHasher));
+        }
+
+        public string HashPassword(TUser user, string password)
+            => _passwordHasher.HashPassword(password);
+
+        public PasswordVerificationResult VerifyHashedPassword(TUser user, string hashedPassword, string providedPassword)
+            => _passwordHasher.VerifyHashedPassword(hashedPassword, providedPassword) ? PasswordVerificationResult.Success : PasswordVerificationResult.Failed;
+    }
+}
